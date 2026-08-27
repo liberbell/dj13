fccac53f8709fd3fb1071a1f42f10687c49a4c21
eliberbell <liberbell@gmail.com>
2026-08-28 08:45:13 +0900

    views4

--- project1/mysite/views.py
+++ project1/mysite/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "mysite/index.html")
+    context = {
+        "title": "Really site"
+    }
+    return render(request, "mysite/index.html", context)
