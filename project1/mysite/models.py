bad1019ec0aadb4a80eb94e96d13769692291024
eliberbell <liberbell@gmail.com>
2026-09-02 08:48:28 +0900

    models2

--- project1/mysite/models.py
+++ project1/mysite/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
