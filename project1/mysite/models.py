5accfe06731dcf4437e7739196de608bb8e573ab
eliberbell <liberbell@gmail.com>
2026-09-02 08:52:43 +0900

    models3

--- project1/mysite/models.py
+++ project1/mysite/models.py
@@ -7,3 +7,16 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None):
         if not email:
             raise ValueError("Users must have an email address")
+        user = self.model(
+            email = self.normalize_email(email),
+        )
+        
+        user.set_passwrod(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+        )
