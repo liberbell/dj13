39080ac3b7c65d637415139a645c0d474cd73cc5
eliberbell <liberbell@gmail.com>
2026-09-02 08:36:38 +0900

    models2

--- project1/mysite/models.py
+++ project1/mysite/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=63)
+    text = models.TextField()
+    author = models.CharField(max_length=63)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
