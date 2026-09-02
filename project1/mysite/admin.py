ea2da72c4994c50cc5c9a49b7ec97a36018e846b
eliberbell <liberbell@gmail.com>
2026-09-03 08:50:56 +0900

    admin1

--- project1/mysite/admin.py
+++ project1/mysite/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from mysite.models import User
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {
+            "fields": (
+                "email",
+                "password",
+            )
+        }),
+        (None, {
+            "fields": (
+                "is_active",
+            )
+        })
+    )
