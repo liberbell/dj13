32354ff43fffb64b777b40ac0f658c14f1d8cf48
eliberbell <liberbell@gmail.com>
2026-09-03 08:31:59 +0900

    models4

--- project1/mysite/models.py
+++ project1/mysite/models.py
@@ -11,7 +11,7 @@ class UserManager(BaseUserManager):
             email = self.normalize_email(email),
         )
         
-        user.set_passwrod(password)
+        user.set_password(password)
         user.save(using=self._db)
         return user
     
