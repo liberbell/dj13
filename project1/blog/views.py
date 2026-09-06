f1fb36452661f7543c64c6fdd88ee598312d1df5
eliberbell <liberbell@gmail.com>
2026-09-06 12:07:25 +0900

    views1

--- project1/blog/views.py
+++ project1/blog/views.py
@@ -4,7 +4,10 @@ from blog.models import Article
 
 # Create your views here.
 def article(request, pk):
+    obj = Article.objects.get(pk=pk)
+    
     context = {
         "pk": pk
     }
+    
     return render(request, "blog/article.html", context)
