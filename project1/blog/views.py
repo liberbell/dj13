491a97e9c7504e5215f554c77bac3ff3e569f3e3
eliberbell <liberbell@gmail.com>
2026-09-08 08:34:18 +0900

    views4

--- project1/blog/views.py
+++ project1/blog/views.py
@@ -10,8 +10,8 @@ def index(request):
     page_num = request.Get.get("page")
     
     context = {
-        "articles": objs,
-        "page_obj": paginator.get_page(page_num)
+        "page_obj": paginator.get_page(page_num),
+        "page_num": page_num,
     }
     
     return render(request, "blog/blogs.html", context)
