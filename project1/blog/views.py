c37ebdb162bbe0c68a1ff45680deeffcf7d09bbd
eliberbell <liberbell@gmail.com>
2026-09-08 08:26:11 +0900

    views2

--- project1/blog/views.py
+++ project1/blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Article
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     objs = Article.objects.all()
+    paginator = Paginator(objs, 2)
+    page_num = request.Get.get("page")
     
     context = {
         "articles": objs
