9b093d1cfd252d96759aa5dd783e0b3a4fe22ad0
eliberbell <liberbell@gmail.com>
2026-09-09 08:56:50 +0900

    views1

--- project1/mysite/views.py
+++ project1/mysite/views.py
@@ -5,7 +5,7 @@ from blog.models import Article
 # Create your views here.
 def index(request):
     
-    objs = Article.objects.all()
+    objs = Article.objects.all()[:3]
     context = {
         "title": "Really site",
         "articles": objs,
