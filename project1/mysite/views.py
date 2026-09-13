a71a31e7e92ec29dd2756ae0ca8131c36b6c60fa
eliberbell <liberbell@gmail.com>
2026-09-14 08:30:35 +0900

    views1

--- project1/mysite/views.py
+++ project1/mysite/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.views import LoginView
 from blog.models import Article
 
 # Create your views here.
@@ -12,11 +12,14 @@ def index(request):
     }
     return render(request, "mysite/index.html", context)
 
-def login(request):
-    context = {
+# def login(request):
+#     context = {
         
-    }
-    if request.method == "POST":
-        context["req"] = request.POST
+#     }
+#     if request.method == "POST":
+#         context["req"] = request.POST
         
-    return render(request, "mysite/login.html", context)
+#     return render(request, "mysite/login.html", context)
+
+class Login(LoginView):
+    template_name = "mysite/login.html"
