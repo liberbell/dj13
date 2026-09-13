aa0da372d7c76cca8f0dd175ae1167ad7bc9927f
eliberbell <liberbell@gmail.com>
2026-09-14 08:31:27 +0900

    urls1

--- project1/config/urls.py
+++ project1/config/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('blog/', include('blog.urls')),
-    path('login/', views.login),
+    path('login/', views.Login.as_view()),
 ]
