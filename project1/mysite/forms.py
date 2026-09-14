bd326986628933e511ca2d8e7b4c84fe22137c89
eliberbell <liberbell@gmail.com>
2026-09-15 08:42:57 +0900

    forms2

--- project1/mysite/forms.py
+++ project1/mysite/forms.py
@@ -3,3 +3,12 @@ from django.contrib.auth import get_user_model
 
 class UserCreationForm(forms.ModelForm):
     password = forms.forms.CharField(max_length=20)
+    
+    class Meta:
+        model = get_user_model()
+        fields = ("email")
+        
+    def clan_password(self):
+        password = self.cleaned_data.get("password")
+        return password
+        
