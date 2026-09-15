aaec551d156d5b0b5587da1fa396e3932f802601
eliberbell <liberbell@gmail.com>
2026-09-16 08:38:12 +0900

    forms1

--- project1/mysite/forms.py
+++ project1/mysite/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth import get_user_model
 
 class UserCreationForm(forms.ModelForm):
-    password = forms.forms.CharField(max_length=20)
+    password = forms.CharField(max_length=20)
     
     class Meta:
         model = get_user_model()
