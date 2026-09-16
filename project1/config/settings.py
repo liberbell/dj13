c76f725e5502c4bb26555190d43c05d74506874e
eliberbell <liberbell@gmail.com>
2026-09-17 08:40:00 +0900

    settings1

--- project1/config/settings.py
+++ project1/config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib import messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,3 +141,11 @@ LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_URL = '/logout/'
 LOGOUT_REDIRECT_URL = '/login/'
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'rounded-0 alert alert-danger',
+    messages.WARNING: 'rounded-0 alert alert-danger',
+    messages.SUCCESS: 'rounded-0 alert alert-danger',
+    messages.INFO: 'rounded-0 alert alert-danger',
+    messages.DEBUG: 'rounded-0 alert alert-danger',
+}
