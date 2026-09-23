d4884949ef1017abaef7f23bf4e1b4e324b5f27b
eliberbell <liberbell@gmail.com>
2026-09-23 12:34:04 +0900

    profile_models3

--- project1/mysite/models/profile_models.py
+++ project1/mysite/models/profile_models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 class Profile(models.Model):
-    user = models.OneToOneField(get_user_model, unique=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(get_user_model, unique=True, on_delete=models.CASCADE, primary_key=True)
+    
     username = models.CharField(default="nemo", max_length=255)
     zipcode = models.CharField(default="", max_length=8)
     prefecture = models.CharField(default="", max_length=50)
