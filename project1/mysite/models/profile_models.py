92cc9a768bd99272e7cc986527dbaf2a9ce3918f
eliberbell <liberbell@gmail.com>
2026-09-23 12:28:51 +0900

    profile_models1

--- project1/mysite/models/profile_models.py
+++ project1/mysite/models/profile_models.py
@@ -4,3 +4,5 @@ class Profile(models.Model):
     username = models.CharField(default="nemo", max_length=255)
     zipcode = models.CharField(default="", max_length=8)
     prefecture = models.CharField(default="", max_length=50)
+    city = models.CharField(default="", max_length=50)
+    address = models.CharField(default="", max_length=100)
